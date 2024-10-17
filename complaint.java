10c820b28c7a41d1bdbfda0a2c428782a878ee74
Krishna Bogineni <113880802+krishnaBogineni-619@users.noreply.github.com>
2024-10-17 14:48:04 +0530

    Add files via upload

---
+++ complaint.java
@@ -0,0 +1,23 @@
+package ecare;
+
+public class complaint{
+    String tockenid;
+    String devicetyoe;
+    String discription;
+    String issue;
+    String specs;
+    public complaint(String tockenid, String devicetyoe, String discription, String issue, String specs){
+        this.tockenid=tockenid;
+        this.devicetyoe=devicetyoe;
+        this.discription=discription;
+        this.issue=issue;
+        this.specs=specs;
+    }
+    
+    
+
+	public String toString() {
+        return "Device Type: "+devicetyoe+"\nDiscription: "+discription+"\nIssue: "+issue+"\nSpecs: "+specs;
+    
+    }
+}
